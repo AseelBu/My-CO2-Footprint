6aafdc9845d04dc85128ea9fac02b05c248c6b5c
AseelBu <aseel_bu@hotmail.com>
2021-04-28 03:21:26 +0300

    changed type to singleton

--- app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/core/DataHolder.java
+++ app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/core/DataHolder.java
@@ -1,4 +1,17 @@
 package com.androidcourse.energyconsumptiondiary_androidapp.core;
 
 public class DataHolder {
+    private static DataHolder instance = null;
+
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private DataHolder(){}
+
+    //Get the only object available
+    public static DataHolder getInstance(){
+        if(instance == null) {
+            instance = new DataHolder();
+        }
+        return instance;
+    }
 }
