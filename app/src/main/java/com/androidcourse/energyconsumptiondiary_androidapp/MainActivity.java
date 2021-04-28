44b91737caac6d93ce46541a7e0223b845f8df28
Maryam <mieriam.wh@gmail.com>
2021-04-28 11:29:22 +0300

    edit in Settings Main activity and update password

--- app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/MainActivity.java
+++ app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/MainActivity.java
@@ -14,34 +14,36 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private ListView list;
     private CustomListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settingsmain);
+        setContentView(R.layout.updatepassword);
 
         List<ItemInfo> itemInfos = new ArrayList<ItemInfo>();
-        itemInfos.add(new ItemInfo("Account Settings",R.drawable.user__1_));
-        itemInfos.add(new ItemInfo("Notification Settings",R.drawable.bell));
-        itemInfos.add(new ItemInfo("share with Friend",R.drawable.share));
+        itemInfos.add(new ItemInfo("Account Settings", R.drawable.user__1_));
+        itemInfos.add(new ItemInfo("Notification Settings", R.drawable.bell));
+        itemInfos.add(new ItemInfo("share with Friend", R.drawable.share));
         itemInfos.add(new ItemInfo());
 //        itemInfos.add(new ItemInfo("Dark Mode",R.drawable.darkmode));
-        list = (ListView) findViewById(R.id.list);
-
-        adapter = new CustomListAdapter(this, itemInfos);
-
-
-        list.setAdapter(adapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                ItemInfo selecteditem = adapter.getItem(position);
-                Toast.makeText(getApplicationContext(), selecteditem.getName(),
-                        Toast.LENGTH_SHORT).show();
-                //adapter.remove(selecteditem);
-            }
-        });
+//        list = (ListView) findViewById(R.id.list);
+//
+//        adapter = new CustomListAdapter(this, itemInfos);
+//
+//
+//        list.setAdapter(adapter);
+//
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                ItemInfo selecteditem = adapter.getItem(position);
+//                Toast.makeText(getApplicationContext(), selecteditem.getName(),
+//                        Toast.LENGTH_SHORT).show();
+//                //adapter.remove(selecteditem);
+//            }
+//        });
+//    }
     }
 }
