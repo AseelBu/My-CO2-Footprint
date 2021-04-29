90ab7a5a9f691e6f12851fa27a601dcf5274d85d
Maryam <mieriam.wh@gmail.com>
2021-04-29 15:34:34 +0300

    adding notification settings,account settings,updating passsword,

--- app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/MainActivity.java
+++ app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/MainActivity.java
@@ -3,31 +3,20 @@ package com.androidcourse.energyconsumptiondiary_androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Context context;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        context=this;
-        Intent intent = new Intent(context, HomePageActivity.class);
-        startActivity(intent);
-        MainActivity.this.finish();
+        setContentView(R.layout.notification);
+//        context=this;
+//        Intent intent = new Intent(context,AcoountSettingsActivity.class);
+//        startActivity(intent);
+//        MainActivity.this.finish();
 
-
-    }
-}
+        }
+        }
