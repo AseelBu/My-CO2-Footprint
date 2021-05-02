26c712d97b11d82891b13f39d2dc4dfb7344043c
AseelBu <aseel_bu@hotmail.com>
2021-05-02 18:06:34 +0300

    updated settings menu, added action bar with return button, added entry class,now leaderboard screen and account settings screen get data from data holder  2

--- app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/NotificationSettingsActivity.java
+++ app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/NotificationSettingsActivity.java
@@ -3,10 +3,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationSettingsActivity extends AppCompatActivity {
@@ -23,10 +26,13 @@ public class NotificationSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.notification);
        entryreminderr= (CheckBox) findViewById(R.id.entryreminderr);
         tips = (CheckBox) findViewById(R.id.tips);
-        returnbtn = (ImageButton) findViewById(R.id.imageButton6);
+//        returnbtn = (ImageButton) findViewById(R.id.imageButton6);
         save = (Button) findViewById(R.id.save);
         context=this;
 
+        ActionBar ab = getSupportActionBar();
+//        ab.setTitle(R.string.yourResults);
+        ab.setDisplayHomeAsUpEnabled(true);
 
     }
 
@@ -39,5 +45,16 @@ public class NotificationSettingsActivity extends AppCompatActivity {
         Intent intent = new Intent(context, SettingsActivity.class);
         startActivity(intent);
 
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return false;
     }
 }
