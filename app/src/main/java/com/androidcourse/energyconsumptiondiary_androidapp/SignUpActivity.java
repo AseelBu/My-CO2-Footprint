733d2eee159bbe8eed1fff913dd9df41d28744fa
Maryam <mieriam.wh@gmail.com>
2021-06-08 16:51:59 +0300

    adding logout in signup

--- app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/SignUpActivity.java
+++ app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/SignUpActivity.java
@@ -146,6 +146,7 @@ public class SignUpActivity extends AppCompatActivity{
         if(user!=null){
             MyCo2FootprintManager.getInstance().openDataBase(this);
             Intent intent = new Intent(this, LogInActivity.class);
+            FirebaseAuth.getInstance().signOut();
             startActivity(intent);
             finish();
         }
