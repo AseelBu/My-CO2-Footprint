d7839cfc25e7a1bc9d8d80877f43842c664faf19
Maryam <mieriam.wh@gmail.com>
2021-06-14 20:18:57 +0300

    merge

--- app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/NotificationSettingsActivity.java
+++ app/src/main/java/com/androidcourse/energyconsumptiondiary_androidapp/NotificationSettingsActivity.java
@@ -82,7 +82,10 @@ public class NotificationSettingsActivity extends AppCompatActivity implements T
         startActivity(intent);
     }
     public void save(View v) {
+        String notiTime=null;
+        if(prefs.getBoolean(getString(R.string.isEntryNotificationSet),false)) {
 
+            notiTime = prefs.getString(getString(R.string.entryNotificationTime), getString(R.string.emptyDash));
 
             Intent notifyIntent = new Intent(this, TimeReceiver.class);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_REMINDER, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -94,6 +97,7 @@ public class NotificationSettingsActivity extends AppCompatActivity implements T
         }
 
         finish();
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
